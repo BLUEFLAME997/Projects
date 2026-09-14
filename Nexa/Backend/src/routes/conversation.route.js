646f84da7198632786f22248ea81a98307cedfe2
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-09-14 18:38:51 +0530

    conversation route

--- Nexa/Backend/src/routes/conversation.route.js
+++ Nexa/Backend/src/routes/conversation.route.js
@@ -1,9 +1,18 @@
 import express from 'express';
-import { createPrivateConversationController } from "../controllers/conversation.controller.js";
+import { createPrivateConversationController, createGroupConversationController } from "../controllers/conversation.controller.js";
 import { authUser } from '../middleware/auth.middleware.js';
 
 const conversationRouter = express.Router();
 
-conversationRouter.post('/private',authUser,createPrivateConversationController);
+/* 
+@route: POST /api/conversation/private
+@description: To get the conversation details if exist or create one
+*/
+conversationRouter.post('/private', authUser, createPrivateConversationController);
+/* 
+@route: POST /api/conversation/group
+@desciption: To create a group conversation with minimum 3 users
+*/
+conversationRouter.post('/group',authUser,createGroupConversationController);
 
 export default conversationRouter;
